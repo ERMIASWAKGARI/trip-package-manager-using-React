0fc4e9d72d7191f5fed59d31d7a0039ef5b5a358
ERMIASWAKGARI <ermiaswakgarisse5@gmail.com>
2023-12-11 10:14:43 -0300

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setquantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setquantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setquantity(Number(e.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
